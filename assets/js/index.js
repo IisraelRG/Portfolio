886749d9542d484ddc8452876980ffad06dd64b5
Israel Gonzalez <89603675+IisraelRG@users.noreply.github.com>
2023-07-28 02:52:41 +0200

    acr

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,8 @@
-menu_show = document.querySelector('.btn_menu')
+menu_show = document.querySelector('.menu-burguer')
 menu = document.querySelector('.menu')
+btn_cv = document.querySelector('.cv')
 
 menu_show.addEventListener('click', ()=>{
-    menu.classList.toggle('menu--show')
+    menu.classList.toggle('menu--show');
+    btn_cv.classList.toggle('cv--show');
 });
